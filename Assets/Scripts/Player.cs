badb36313b13589d0557d8da9a4dfd79e1c1eff2
Darius Milea <37295547+OpiumCodes@users.noreply.github.com>
2019-01-15 19:15:55 +0200

    Added everything
    Error with ground collider, ground check needed for Jump() function

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private KeyCode moveUp;
+    private KeyCode moveDown;
+    private KeyCode moveRight;
+    private KeyCode moveLeft;
+    private int speed = 12;
+
+    private Vector2 touchOrigin = -Vector2.one;
+    // Use this for initialization
+    void Start()
+    {
+        moveUp = KeyCode.UpArrow;
+        moveDown = KeyCode.DownArrow;
+        moveRight = KeyCode.RightArrow;
+        moveLeft = KeyCode.LeftArrow;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void MoveUp(Rigidbody2D rb)
+    {
+        rb.velocity = new Vector2(0, speed);
+    }
+   
+    void MoveLeft(Rigidbody2D rb)
+    {
+         rb.velocity = new Vector2(speed / 2, 0);
+    }
+    void MoveRight(Rigidbody2D rb)
+    {
+        rb.velocity = new Vector2(-1*speed / 2, 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(moveUp))
+        {
+            rb.velocity = new Vector2(0, speed);
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            rb.velocity = new Vector2(0, speed * -1);
+        }
+        else if(Input.GetKey(moveRight))
+        {
+            rb.velocity = new Vector2(speed / 2, 0);
+        }
+        else if (Input.GetKey(moveLeft))
+        {
+            rb.velocity = new Vector2(-1*speed / 2, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10f, jumpvelocity = 10f;
+    private Transform myTransform,tagGround;
+    private Rigidbody2D myRB;
+    private LayerMask playerMask;
+    public bool isGround = false;
+    void Start()
+    {
+        tagGround = GameObject.Find("tag_ground").transform;
+        if (tagGround != null)
+            Debug.Log("Found tag_ground");
+        myRB = GetComponent<Rigidbody2D>();
+        myTransform = this.transform;
+    }
+
+    void FixedUpdate()
+    {
+        isGround = Physics2D.Linecast(myTransform.position, tagGround.position);
+        if(isGround==false)
+        {
+            Debug.Log("Not Touching Ground");
+        }
+        else
+        {
+            Debug.Log("Touching ground");
+        }
+        Move(Input.GetAxisRaw("Horizontal"));
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+    public void Move(float horizontalInput)
+    {
+        myRB.velocity = new Vector2(horizontalInput*speed,0);
+    }
+    public void Jump()
+    {
+        if(isGround)
+        myRB.velocity += jumpvelocity*Vector2.up;
+    }
+}
