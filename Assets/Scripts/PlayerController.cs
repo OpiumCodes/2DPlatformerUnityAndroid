819c6080d1c8dabf0236937fcd0ead6221fceef3
Darius Milea <37295547+OpiumCodes@users.noreply.github.com>
2019-01-20 12:12:44 +0200

    Adaugat comentarii si explicatii la PlayerController
    Adaugat ce fac variabilele si functiile
    Fixed GroundCheck
    Eroare la Jump cu buton din UI necesita fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,44 +5,45 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 10f, jumpvelocity = 300f;
-    private Transform myTransform,tagGround;
-    private Rigidbody2D myRB;
-    public LayerMask Ground;
-    public Transform GroundCheck;
-    private float groundCheckRadius = 0.1f;
-    public bool Grounded;
-    private float hInput =0;
+    public float speed = 10f, jumpvelocity = 300f;//Speed se adauga la vectorul de velocity al caracterului pentru a-l misca cu atatia pixeli jump la fel
+    private Transform myTransform;//Asta e un fel de state of an object
+    private Rigidbody2D myRB;//Asta e playerul principal
+    public LayerMask Ground;//Cu asta arat indicatorului de ground ce este un ground, adica un layer in plus ce il pun peste obiectele ce le consider ground
+    public Transform GroundCheck;//Asta e indicator daca e grounded sau nu
+    private float groundCheckRadius = 0.1f;//Asta e radius-ul checkului de ground adica daca ceva din layerul ground intra in acel cerc se schimba starea sa
+    public bool Grounded;//Asta e indicele de grounded daca e activ se poate apela Jump
+    private float hInput =0;//Asta il folosesc pentru a face legatura dintre butoane si functia move
     void Start()
     {
-        myRB = GetComponent<Rigidbody2D>();
-        myTransform = this.transform;
+        myRB = GetComponent<Rigidbody2D>();//Player
+        myTransform = this.transform;//Starea Player
     }
 
     void FixedUpdate()
     {
+        Debug.Log("Velocity="+myRB.velocity);
         Grounded = Physics2D.OverlapCircle(GroundCheck.position, groundCheckRadius, Ground);
-#if UNITY_ADROID
-        Move(Input.GetAxisRaw("Horizontal"));
-        if (Input.GetButtonDown("Jump"))
+#if !UNITY_ADROID//Cu asta verific daca e pe android sau nu, adica pe desktop sa mearga butoanele si pe android doar alea de pe ecran
+        Move(Input.GetAxisRaw("Horizontal"));//Asta verifica daca e input pe axa stanga dreapta si o apeleaza
+        if (Input.GetButtonDown("Jump"))//Asta verifica daca e input default de jump adica space sau sageata sus
         {
-            Jump();
+            Jump();//Functia de jump definita mai jos
         }
 #else
-        Move(hInput);
+        Move(hInput);//Asta e functia speciala pentru mutat cu input din UI care practic face acelasi lucru ca si Move doar ca tine minte o stare ca sa se miste smooth
 #endif
 
     }
     void Move(float horizontalInput)
     {
-        myRB.velocity = new Vector2(horizontalInput*speed,0);
+        myRB.velocity = new Vector2(horizontalInput*speed,0);//Adauga le vectorul de velocity input orizontal adica -1 sau +1 stanga/dreapta * viteza pe axa stanga dreapta
     }
     public void Jump()
     {
         Debug.Log("Jump pressed");
-        if (Grounded)
+        if (Grounded)//Daca e grounded
         {
-            myRB.velocity += jumpvelocity * Vector2.up;
+            myRB.velocity += jumpvelocity * Vector2.up;//Inmulteste velocity de sus cu cat vrei sa sara in float 
             Debug.Log(myRB.velocity);
         }
 
@@ -50,6 +51,6 @@ public class PlayerController : MonoBehaviour
 
     public void StartMoving(float horizontalInput)
     {
-        hInput = horizontalInput;
+        hInput = horizontalInput;//Asta e doar sa retina starea de move sau stay ca altfel e snappy pentru butoane de UI
     }
 }
