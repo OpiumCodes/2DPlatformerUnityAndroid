b9b9aa8aba80393360440c2ac6853189a067b6ec
Darius Milea <37295547+OpiumCodes@users.noreply.github.com>
2019-01-21 11:49:44 +0200

    Added Menu and falling death
    Fixed jump issue
    Added a menu with play and exit button
    Added a button to go to the menu in game UI
    Added a death by falling out of the map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float speed = 10f, jumpvelocity = 300f;//Speed se adauga la vectorul de velocity al caracterului pentru a-l misca cu atatia pixeli jump la fel
+    public float speed = 10f, jumpvelocity = 60f;//Speed se adauga la vectorul de velocity al caracterului pentru a-l misca cu atatia pixeli jump la fel
     private Transform myTransform;//Asta e un fel de state of an object
     private Rigidbody2D myRB;//Asta e playerul principal
     public LayerMask Ground;//Cu asta arat indicatorului de ground ce este un ground, adica un layer in plus ce il pun peste obiectele ce le consider ground
@@ -13,24 +12,27 @@ public class PlayerController : MonoBehaviour
     private float groundCheckRadius = 0.1f;//Asta e radius-ul checkului de ground adica daca ceva din layerul ground intra in acel cerc se schimba starea sa
     public bool Grounded;//Asta e indicele de grounded daca e activ se poate apela Jump
     private float hInput =0;//Asta il folosesc pentru a face legatura dintre butoane si functia move
+    private Vector2 startPoint;
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();//Player
         myTransform = this.transform;//Starea Player
+        startPoint = myTransform.position;
     }
 
     void FixedUpdate()
     {
         Debug.Log("Velocity="+myRB.velocity);
         Grounded = Physics2D.OverlapCircle(GroundCheck.position, groundCheckRadius, Ground);
-#if !UNITY_ADROID//Cu asta verific daca e pe android sau nu, adica pe desktop sa mearga butoanele si pe android doar alea de pe ecran
+#if UNITY_ADROID//Cu asta verific daca e pe android sau nu, adica pe desktop sa mearga butoanele si pe android doar alea de pe ecran
         Move(Input.GetAxisRaw("Horizontal"));//Asta verifica daca e input pe axa stanga dreapta si o apeleaza
         if (Input.GetButtonDown("Jump"))//Asta verifica daca e input default de jump adica space sau sageata sus
         {
             Jump();//Functia de jump definita mai jos
         }
 #else
-        Move(hInput);//Asta e functia speciala pentru mutat cu input din UI care practic face acelasi lucru ca si Move doar ca tine minte o stare ca sa se miste smooth
+        if(hInput!=0)
+            Move(hInput);//Asta e functia speciala pentru mutat cu input din UI care practic face acelasi lucru ca si Move doar ca tine minte o stare ca sa se miste smooth
 #endif
 
     }
@@ -52,5 +54,14 @@ public class PlayerController : MonoBehaviour
     public void StartMoving(float horizontalInput)
     {
         hInput = horizontalInput;//Asta e doar sa retina starea de move sau stay ca altfel e snappy pentru butoane de UI
+    }
+    
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="killbox")
+        {
+            myRB.position = startPoint;
+        }
     }
 }
